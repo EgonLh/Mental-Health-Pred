40380c25b559dfd65078b5f39e5cd665e47e7ad2
egon <121918108+EgonLh@users.noreply.github.com>
2025-06-03 09:44:41 +0630

    fixed:minor errors

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
-import joblib  # use joblib instead of pickle
+import joblib 
 
 app = FastAPI()
 
-# Load trained model and encoder using joblib
 model = joblib.load("../models/mental_health_model.pkl")
 ordinal_encoder = joblib.load("../models/ordinal_encoder.pkl")
 
-# Define input schema
 class SurveyData(BaseModel):
     gender: str
     self_employed: str
@@ -22,20 +20,17 @@ class SurveyData(BaseModel):
     coping_struggles: str
     work_interest: str
 
-# Define endpoint
 @app.post("/predict")
 def predict(data: SurveyData):
-    # Preprocess input (lowercase + underscore)
     input_data = {
         k: v.lower().replace(" ", "_") for k, v in data.dict().items()
     }
 
-    # Manually encode binary columns: yes=1, no=0
+    #  yes=1, no=0
     binary_cols = ['gender', 'self_employed', 'family_history', 'coping_struggles']
     for col in binary_cols:
         input_data[col] = 1 if input_data[col] == 'yes' else 0
 
-    # Ordinal columns — use loaded encoder
     ordinal_input = [
         input_data['days_indoors'],
         input_data['growing_stress'],
@@ -45,8 +40,6 @@ def predict(data: SurveyData):
         input_data['work_interest']
     ]
     encoded_ordinal = ordinal_encoder.transform([ordinal_input])
-
-    # Construct final input for model
     final_input = [
         input_data['gender'],
         input_data['self_employed'],
